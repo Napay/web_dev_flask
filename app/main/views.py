1d8992a2559109c878e31a27f76f2d2c21a67218
Napay <johnbenedictdimero@gmail.com>
2020-08-17 11:36:29 +0800

    08/17/2020

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,26 @@ from flask_login import login_required, current_user
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
 from ..decorators import admin_required, permission_required
+from flask_sqlalchemy import get_debug_queries
+
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                % (query.statement, query.parameters, query.duration, query.context))
+    return response
+
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
 
 @main.route('/',methods=['GET', 'POST'])
 def index():
--- manage.py
+++ manage.py
@@ -1,4 +1,12 @@
 import os
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+	import coverage
+	COV = coverage.coverage(branch=True, include='app/*')
+	COV.start()
+
+import sys
 import click
 from app import create_app, db
 from app.models import User, Role, Post, Permission, Follow, Comment
@@ -17,6 +25,7 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+
 # @app.cli.command()
 # @click.argument('test_names', nargs=-1)
 # def test(test_names):
@@ -28,12 +37,42 @@ manager.add_command('db', MigrateCommand)
 # 		tests = unittest.TestLoader().discover('tests')
 # 	unittest.TextTestRunner(verbosity=2).run(tests)
 
+# @manager.command
+# def test():
+# 	'''Run the unit tests'''
+# 	import unittest
+# 	tests = unittest.TestLoader().discover('tests')
+# 	unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager.command
-def test():
-	'''Run the unit tests'''
+def test(coverage=False):
+	"""Run the unit tests"""
+	if coverage and not os.environ.get('FLASK_COVERAGE'):
+		import subprocess
+		os.environ['FLASK_COVERAGE'] = '1'
+		sys.exit(subprocess.call(sys.argv))
+
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
+	if COV:
+		COV.stop()
+		COV.save()
+		print('Coverage Summary:')
+		COV.report()
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		covdir = os.path.join(basedir, 'tmp/coverage')
+		COV.html_report(directory=covdir)
+		print('HTML version: file://%s/index.html' % covdir)
+		COV.erase()
+
+@manager.command
+def profile(length=25, profile_dir=None):
+	"""Start the application under the code profiler."""
+	from werkzeug.contrib.profiler import ProfilerMiddleware
+	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+								profile_dir=profile_dir)
+	app.run(debug=False)
 
 
 if __name__=='__main__':
